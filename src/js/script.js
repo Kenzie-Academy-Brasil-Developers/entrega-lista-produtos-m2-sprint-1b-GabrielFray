9e7c1d94514441489a9cfbcd0a83d9cd6b42233a
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-06-01 11:29:40 -0300

    confirmar

--- src/js/script.js
+++ src/js/script.js
@@ -117,60 +117,94 @@ function btnMostarTudo() {
 //   const valorTotal = document.querySelector("#precoTotal");
 //   valorTotal.innerText = `R$ ${price}.00`;
 // }
+function criarProdutoCart(itens) {
+  const cart = document.querySelector(".cart");
 
-// let carrinho = [];
-// addEventListener("click", (event) => {
-//   const clickTarget = event.target;
-//   if (clickTarget.className === "btn-add") {
-//     carrinho.push(data[Number(clickTarget.id - 1)]);
-//     cart.innerHTML = "";
-//     carrinho.map((element, index) => {
-//       createCart(element, index);
-//     });
-//   }
-//   if (clickTarget.className === "btn-remove") {
-//     carrinho.splice(Number(clickTarget.id - 1), 1);
-//     cart.innerHTML = "";
-//     carrinho.map((element, index) => {
-//       createCart(element, index);
-//     });
-//   }
-//   if (carrinho.length > 0) {
-//     const price = carrinho.reduce((acc, adedonha) => {
-//       return acc + adedonha.value;
-//     }, 0);
-//     const total = document.querySelector(".emptyTotal");
-//     total.innerHTML = "";
-//     total.insertAdjacentHTML(
-//       "beforeend",
-//       `<div class="total">
-//       <p>Quantidade: <span>${carrinho.length}</span></p>
-//       <p>Total: <span>R$${price}.00</span></p>
-//       </div>
-//       `
-//     );
-//   }
-//   if (carrinho.length === 0) {
-//     const total = document.querySelector(".emptyTotal");
-//     total.innerHTML = "";
-//     cart.innerHTML = "";
-//     cart.insertAdjacentHTML(
-//       "beforeend",
-//       `
-//       <img class="img-cart" src="img/logo-bigorna.svg" alt="" />
-//       <p>Adicione suas espadas</p>
-//       `
-//     );
-//   }
-// });
-// cart.insertAdjacentHTML(
-//   "beforeend",
-//   `
-//   <img class="img-cart" src="img/logo-bigorna.svg" alt="" />
-//   <p>Adicione suas espadas</p>
+  const liCart = document.createElement("li");
+  const imgCart = document.createElement("img");
+  const divCartInfos = document.createElement("div");
+  const tituloCart = document.createElement("h3");
+  const paragrafoCart = document.createElement("p");
+  const precoCart = document.createElement("span");
+  const btnLixeira = document.createElement("button");
+  const imgLixeira = document.createElement("img");
 
-//   `
-// );
+  imgCart.src = itens.img;
+  console.log(itens.img);
+  imgCart.alt = itens.nome;
+  console.log(itens.nome);
+  tituloCart.innerText = itens.nome;
+  paragrafoCart.innerText = itens.secao;
+  precoCart.innerText = `R$${itens.preco}`;
+  imgLixeira.src = "src/img/Group 1033.png";
+
+  liCart.classList.add("card--carrinho");
+  divCartInfos.classList.add("card--infos");
+  btnLixeira.classList.add("cart--lixeira");
+
+  cart.append(liCart);
+
+  liCart.append(imgCart, divCartInfos, btnLixeira);
+  divCartInfos.append(tituloCart, paragrafoCart, precoCart);
+  btnLixeira.append(imgLixeira);
+
+  return cart;
+}
+criarProdutoCart(produtos);
+
+function renderizarCarrinho(productsArray) {
+  const cartLista = document.querySelector(".cart");
+  productsArray.forEach((element) => {
+    const productsList = criarProdutoCart(element);
+    cartLista.append(productsList);
+  });
+}
+renderizarCarrinho(produtos);
+
+let carrinho = [];
+addEventListener("click", (event) => {
+  const clickTarget = event.target;
+  if (clickTarget.className === "btnAddCart") {
+    carrinho.push(produtos[Number(clickTarget.id - 1)]);
+    carrinho.map((element, index) => {
+      renderizarCarrinho(element, index);
+    });
+  }
+  // if (clickTarget.className === "btn-remove") {
+  //   carrinho.splice(Number(clickTarget.id - 1), 1);
+  //   cart.innerHTML = "";
+  //   carrinho.map((element, index) => {
+  //     createCart(element, index);
+  //   });
+  // }
+  // if (carrinho.length > 0) {
+  //   const price = carrinho.reduce((acc, adedonha) => {
+  //     return acc + adedonha.value;
+  //   }, 0);
+  //   const total = document.querySelector(".emptyTotal");
+  //   total.innerHTML = "";
+  //   total.insertAdjacentHTML(
+  //     "beforeend",
+  //     `<div class="total">
+  //     <p>Quantidade: <span>${carrinho.length}</span></p>
+  //     <p>Total: <span>R$${price}.00</span></p>
+  //     </div>
+  //     `
+  //   );
+  // }
+  // if (carrinho.length === 0) {
+  //   const total = document.querySelector(".emptyTotal");
+  //   total.innerHTML = "";
+  //   cart.innerHTML = "";
+  //   cart.insertAdjacentHTML(
+  //     "beforeend",
+  //     `
+  //     <img class="img-cart" src="img/logo-bigorna.svg" alt="" />
+  //     <p>Adicione suas espadas</p>
+  //     `
+  //   );
+  // }
+});
 
 function chamadaDasFuncoes() {
   renderizarCard(produtos);
